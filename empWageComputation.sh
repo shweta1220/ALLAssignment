a51ab1b9691d26036e64b6f3df7baaaac31c35ad
shweta <shwetalad574@gmail.com>
2020-05-22 20:52:18 +0530

    Employee code added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,13 @@
-echo "Welcome"
+#!/bin/bash -x
+isPartime=1
+isAbsent=0
+empRandomCheck=$((RANDOM % 2))
+if [ $isPresent -eq $empRandomCheck ]
+then
+    echo "Employee is Present"
+else [ $isAbsent -eq $empRandomCheck ]
+then
+    echo "Employee is Absent"
+fi
+
+
