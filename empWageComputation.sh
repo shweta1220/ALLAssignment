01952ded6ad66e0bd8d1b8722c6422d7fbcaa419
shweta <shwetalad574@gmail.com>
2020-05-22 21:03:38 +0530

    Employee wages added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -x
 isPartime=1
 isAbsent=0
+empWagePerHour=20
+empHours=8
 empRandomCheck=$((RANDOM % 2))
 if [ $isPresent -eq $empRandomCheck ]
 then
-    echo "Employee is Present"
+     "Employee is Present"
 else [ $isAbsent -eq $empRandomCheck ]
 then
-    echo "Employee is Absent"
+     "Employee is Absent"
 fi
+empWage=$(( $empHours * $empWagePerHour ))
 
 
