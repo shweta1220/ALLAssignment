df104d83a5ad7f0af316f3f501161fa25e696389
shweta <shwetalad574@gmail.com>
2020-06-03 21:28:38 +0530

    part time employee added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,7 +2,8 @@ class EmployeeWage{
 
 public static void main(String []args)
      {
-		int IS_FULL_TIME = 1;
+		int IS_FULL_TIME = 2;
+                int IS_PART_TIME = 1;
                 int Emp_Rate_Per_Hr = 20;
                 int empHrs = 0;
                 int empWage = 0; 
@@ -10,12 +11,13 @@ double empCheck = Math.floor(Math.random() * 10) % 2;
 		System.out.println("Welcome");
 if (empCheck == IS_FULL_TIME)
 empHrs=8;
-System.out.println("Employee is Present");
-else
+else if (empCheck == IS_PART_TIME)
+empHrs=4;
+else 
 empHrs=0;
 empWage = empHrs * (Emp_Rate_Per_Hr);
 System.out.println("Emp Wage:" * empWage);
-System.out.println("Employee is Abscent");
+
      }
 
 
