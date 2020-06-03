7a7e716c6ae54960157daf14cab1e054137716d4
shweta <shwetalad574@gmail.com>
2020-06-03 21:45:30 +0530

    monthly employee wage added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,6 +5,7 @@ public static void main(String []args)
 		int IS_FULL_TIME = 2;
                 int IS_PART_TIME = 1;
                 int Emp_Rate_Per_Hr = 20;
+                int Emp_Day_Per_Month = 20; 
                 int empHrs = 0;
                 int empWage = 0; 
 double empCheck = Math.floor(Math.random() * 10) % 2;
@@ -16,9 +17,10 @@ empHrs=4;
 else 
 empHrs=0;
 empWage = empHrs * (Emp_Rate_Per_Hr);
+empWageMonth = empWage * Emp_Day_Per_Month;
 System.out.println("Emp Wage:" * empWage);
 
-     }
+     }  
 
 
 
