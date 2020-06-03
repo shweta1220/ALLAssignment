2a3b21f16edbc88ed39841f8311b4460b5d9cc9b
shweta <shwetalad574@gmail.com>
2020-06-03 19:33:54 +0530

    Employee Attendance added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,8 +2,13 @@ class EmployeeWage{
 
 public static void main(String []args)
      {
-		//printing the message
+		int IS_FULL_TIME = 1;
+double empCheck = Math.floor(Math.random() * 10) % 2;
 		System.out.println("Welcome");
+if (empCheck == IS_FULL_TIME)
+System.out.println("Employee is Present");
+else
+System.out.println("Employee is Abscent");
      }
 
 }
